7e8f0c6523c9ff1b1f6c907c6049d0e5ce2efbe2
Jose Garcia <66678529+seiuro2@users.noreply.github.com>
2020-10-17 19:33:15 -0500

    cambios: js se modifico a for.each la funcion que hacia render, modificacion de classes en css

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,27 +1,43 @@
-
 let users = [];
 const calue = setTimeout(() => {
   console.log('hola a los 5 segundos');
 }, 5000);
 
 function getUsers() {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(
-      (response) => response.json())
-    .then(
-      (json) => {
-
-        renderUsers(json)
-      }
-    );
-
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => {
+      /*   renderUsers(json) */
+      console.log(json);
+      renderUsers(json);
+    });
 }
 
-
-
 function renderUsers(usersList) {
-  let container = document.getElementById("users")
-  for (let i = 0; i < usersList.length; i++) {
+  users = usersList;
+  const container = document.getElementById('userContainer');
+  usersList.forEach((user) => {
+    const template = ` <div class="user"  data-id="${user.id}" data-name="${user.name}" id= "user-${user.id}">
+        <p>${user.name} <br> ${user.username} </p>
+        <p>${user.email} <br> ${user.address.street} </p>
+      </div>`;
+    container.innerHTML += template;
+    let userElement = document.getElementById(`user-${user.id}`);
+
+    /*  console.log(template); */
+  });
+  clickElement();
+  /* let newList = [];
+  usersList.forEach(function (user, index) {
+    newList.push(user.phone);
+  });
+  console.log(newList); */
+  /* let newList = usersList.map(function (user) {
+    return user.phone; 
+  });
+  console.log(newList); */
+
+  /*  for (let i = 0; i < usersList.length; i++) {
     let user = usersList[i];
     //console.log(user);
     let textNote = document.createTextNode(user)
@@ -32,18 +48,19 @@ function renderUsers(usersList) {
     const template =
 
       ` <div id="users">
-        <p>${user.userID} <br> ${user.id} </p>
-        <p>${user.title} <br> ${user.completed} </p>
+        <p>${user.name} <br> ${user.username} </p>
+        <p>${user.email} <br> ${user.addres} </p>
       </div>`
     container.innerHTML += template
-    console.log(template);
-
-
-
-
-  }
+    console.log(template);*/
 }
-
+function clickElement() {
+  const userElements = document.getElementsByClassName('user');
+  Array.from(userElements).forEach(function (user) {
+    user.addEventListener('click', function () {
+      console.log('click', Number(this.dataset.id));
+    });
+  });
+}
+function showUser(userID) {}
 getUsers();
-
-
